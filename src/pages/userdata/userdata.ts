d906eccc19edc4dcf3dbe502a4989edf9fbff2b9
Oliver Gilbey <olliegilbey@gmail.com>
2018-08-19 05:02:15 +0200

    bar graph week

--- src/pages/userdata/userdata.ts
+++ src/pages/userdata/userdata.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Chart } from 'chart.js';
+import { ChartDataProvider } from '../../providers/chartdata/charts';
 
 /**
  * Generated class for the UserdataPage page.
@@ -15,11 +17,53 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class UserdataPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('barCanvas') barCanvas;
+
+  barChart: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public chartdata:ChartDataProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UserdataPage');
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+
+      type: 'bar',
+      data: {
+        labels: this.chartdata.fetchWeekLabels(),
+        datasets: [{
+          label: '# of Votes',
+          data: this.chartdata.fetchWeekData(),
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true
+            }
+          }]
+        }
+      }
+
+    });
   }
 
 }
